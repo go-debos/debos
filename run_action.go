43dd3ac4cf2c3768cd6d6094bf5e62937d88464b
Sjoerd Simons <sjoerd.simons@collabora.co.uk>
2017-07-11 15:15:58 +0200

    Allow run action to run at postprocessing time

--- run_action.go
+++ run_action.go
@@ -4,13 +4,21 @@ import (
 	"fmt"
 	"github.com/sjoerdsimons/fakemachine"
 	"path"
+	"log"
 )
 
 type RunAction struct {
 	*BaseAction
 	Chroot  bool
+	PostProcess  bool
 	Script  string
 	Command string
+}
+
+func (run *RunAction) Verify(context YaibContext) {
+	if run.PostProcess && run.Chroot {
+		log.Fatal("Cannot use both chroot and postprocess in a run action")
+	}
 }
 
 func (run *RunAction) PreMachine(context *YaibContext, m *fakemachine.Machine,
@@ -21,10 +29,12 @@ func (run *RunAction) PreMachine(context *YaibContext, m *fakemachine.Machine,
 	}
 
 	run.Script = CleanPathAt(run.Script, context.recipeDir)
-	m.AddVolume(path.Dir(run.Script))
+	if ! run.PostProcess {
+		m.AddVolume(path.Dir(run.Script))
+	}
 }
 
-func (run *RunAction) Run(context *YaibContext) {
+func (run *RunAction) doRun(context YaibContext) {
 	var command string
 
 	if run.Script != "" {
@@ -41,7 +51,7 @@ func (run *RunAction) Run(context *YaibContext) {
 	var err error
 	if run.Chroot {
 		if run.Script != "" {
-			q := NewQemuHelper(*context)
+			q := NewQemuHelper(context)
 			q.Setup()
 			defer q.Cleanup()
 
@@ -51,14 +61,31 @@ func (run *RunAction) Run(context *YaibContext) {
 			options = append(options, command)
 			RunCommand(path.Base(run.Script), "systemd-nspawn", options...)
 		} else {
-			err = RunCommandInChroot(*context, command, "sh", "-c", command)
+			err = RunCommandInChroot(context, command, "sh", "-c", command)
 		}
 	} else {
-		command = command + " " + context.rootdir
+		if ! run.PostProcess {
+			command = command + " " + context.rootdir
+		}
 		RunCommand(path.Base(command), "sh", "-c", command)
 	}
 
 	if err != nil {
 		panic(err)
 	}
+}
+
+func (run *RunAction) Run(context *YaibContext) {
+	if run.PostProcess {
+		/* This runs in postprocessing instead */
+		return
+	}
+	run.doRun(*context)
+}
+
+func (run *RunAction) PostMachine(context YaibContext) {
+	if ! run.PostProcess {
+		return
+	}
+	run.doRun(context)
 }
