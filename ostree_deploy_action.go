03bb2f89c064b49afab78baec23e79abb85e7d19
Sjoerd Simons <sjoerd.simons@collabora.co.uk>
2017-07-28 14:44:24 +0200

    Import image fstab and kernel cmdline into the ostree deployment

--- ostree_deploy_action.go
+++ ostree_deploy_action.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
+	"os"
 	"path"
+	"strings"
 
 	ostree "github.com/sjoerdsimons/ostree-go/pkg/otbuiltin"
 )
@@ -56,8 +61,38 @@ func (ot *OstreeDeployAction) Run(context *YaibContext) {
 		log.Fatalf("%s", err)
 	}
 
+	cmdline, _ := ioutil.ReadFile(path.Join(context.imageMntDir, "etc/kernel/cmdline"))
+	kargs := strings.Split(strings.TrimSpace(string(cmdline)), " ")
+
 	origin := sysroot.OriginNewFromRefspec(ot.Branch)
-	deployment, err := sysroot.DeployTree(ot.Os, revision, origin, nil, nil, nil)
+	deployment, err := sysroot.DeployTree(ot.Os, revision, origin, nil, kargs, nil)
+	if err != nil {
+		log.Fatalf("%s", err)
+	}
+
+	deploymentDir := fmt.Sprintf("ostree/deploy/%s/deploy/%s.%d",
+		deployment.Osname(), deployment.Csum(), deployment.Deployserial())
+
+	etcDir := path.Join(context.imageMntDir, deploymentDir, "etc")
+
+	err = os.Mkdir(etcDir, 755)
+	if err != nil && !os.IsExist(err) {
+		log.Fatalf("%s", err)
+	}
+
+	dst, err := os.OpenFile(path.Join(etcDir, "fstab"), os.O_WRONLY|os.O_CREATE, 0755)
+	defer dst.Close()
+	if err != nil {
+		log.Fatalf("%s", err)
+	}
+
+	src, err := os.Open(path.Join(context.imageMntDir, "etc", "fstab"))
+	defer src.Close()
+	if err != nil {
+		log.Fatalf("%s", err)
+	}
+
+	_, err = io.Copy(dst, src)
 	if err != nil {
 		log.Fatalf("%s", err)
 	}
