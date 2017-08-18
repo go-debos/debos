2e1dbe0e0babb766d8c948cb9cd61f14879a2b2a
Denis Pynkin <denis.pynkin@collabora.com>
2017-08-18 17:53:33 +0300

    Action 'unpack' refactored
    - Move unpack of tar archive to dedicated function 'UnpackTarArchive' to allow
      use it not only with 'unpack' action.
    - Add support of 'bzip2' and 'xz' compression types.
    - Add possibility to pass additional arguments for 'tar'.
    
    Signed-off-by: Denis Pynkin <denis.pynkin@collabora.com>

--- unpack_action.go
+++ unpack_action.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"path"
@@ -12,12 +13,45 @@ type UnpackAction struct {
 	File        string
 }
 
+func tarOptions(compression string) string {
+	unpackTarOpts := map[string]string{
+		"gz":    "-z",
+		"bzip2": "-j",
+		"xz":    "-J",
+	} // Trying to guess all other supported formats
+
+	return unpackTarOpts[compression]
+}
+
+func UnpackTarArchive(infile, destination, compression string, options ...string) error {
+	if err := os.MkdirAll(destination, 0755); err != nil {
+		return err
+	}
+	log.Printf("Unpacking %s\n", infile)
+
+	command := []string{"tar"}
+	command = append(command, options...)
+	command = append(command, "-x")
+	if unpackTarOpt := tarOptions(compression); len(unpackTarOpt) > 0 {
+		command = append(command, unpackTarOpt)
+	}
+	command = append(command, "-f", infile, "-C", destination)
+
+	return Command{}.Run("unpack", command...)
+}
+
+func (pf *UnpackAction) Verify(context *DebosContext) error {
+	unpackTarOpt := tarOptions(pf.Compression)
+	if len(pf.Compression) > 0 && len(unpackTarOpt) == 0 {
+		return fmt.Errorf("Compression '%s' is not supported.\n", pf.Compression)
+	}
+
+	return nil
+}
+
 func (pf *UnpackAction) Run(context *DebosContext) error {
 	pf.LogStart()
 	infile := path.Join(context.artifactdir, pf.File)
 
-	os.MkdirAll(context.rootdir, 0755)
-
-	log.Printf("Unpacking %s\n", infile)
-	return Command{}.Run("unpack", "tar", "xzf", infile, "-C", context.rootdir)
+	return UnpackTarArchive(infile, context.rootdir, pf.Compression)
 }
