4838bccc1e252b76f6ede701d26613ce55f5b4af
Peter Senna Tschudin <peter.senna@collabora.com>
2018-08-16 12:00:36 +0200

    net.go: Rename url to downloadURL
    In order to implement the http_proxy functionality it is necessary to
    use the net/url library. Having a variable named url causes issues, and
    this patch renames the variable url to downloadUrl.
    
    Signed-off-by: Peter Senna Tschudin <peter.senna@collabora.com>

--- net.go
+++ net.go
@@ -9,25 +9,25 @@ import (
 )
 
 // Function for downloading single file object with http(s) protocol
-func DownloadHttpUrl(url, filename string) error {
-	log.Printf("Download started: '%s' -> '%s'\n", url, filename)
+func DownloadHttpUrl(downloadUrl, filename string) error {
+	log.Printf("Download started: '%s' -> '%s'\n", downloadUrl, filename)
 
 	// TODO: Proxy support?
 
 	// Check if file object already exists.
 	fi, err := os.Stat(filename)
 	if !os.IsNotExist(err) && !fi.Mode().IsRegular() {
-		return fmt.Errorf("Failed to download '%s': '%s' exists and it is not a regular file\n", url, filename)
+		return fmt.Errorf("Failed to download '%s': '%s' exists and it is not a regular file\n", downloadUrl, filename)
 	}
 
-	resp, err := http.Get(url)
+	resp, err := http.Get(downloadUrl)
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("Url '%s' returned status code %d (%s)\n", url, resp.StatusCode, http.StatusText(resp.StatusCode))
+		return fmt.Errorf("Url '%s' returned status code %d (%s)\n", downloadUrl, resp.StatusCode, http.StatusText(resp.StatusCode))
 	}
 
 	// Output file
