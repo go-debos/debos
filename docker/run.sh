7b156f727b4d953c4837e8ee5db63517eb6cf8d0
Maciej Pijanowski <maciej.pijanowski@3mdeb.com>
2018-08-22 21:25:22 +0200

    docker/run.sh: drop 'privileged', expose kvm device
    Signed-off-by: Maciej Pijanowski <maciej.pijanowski@3mdeb.com>

--- docker/run.sh
+++ docker/run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-docker run --rm \
-	-it \
-	--privileged \
-	-v ${PWD}:/root \
-	debos \
-	/bin/bash -c "debos $*"
+docker run --rm -it \
+    -u $(id -u) \
+    --device /dev/kvm \
+    --group-add=$(getent group kvm | cut -d : -f 3) \
+    -v ${PWD}:/root \
+    debos \
+    /bin/bash -c "debos $*"
