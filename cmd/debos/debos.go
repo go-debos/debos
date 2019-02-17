af3a65297bbc554bc00aeb31b175662c6d3d0e94
Frédéric Danis <frederic.danis@collabora.com>
2019-02-17 10:38:24 +0100

    cmd: Fix dump of void slice
    Signed-off-by: Frédéric Danis <frederic.danis@collabora.com>

--- cmd/debos/debos.go
+++ cmd/debos/debos.go
@@ -110,7 +110,7 @@ func DumpActionFields(iface interface{}, depth int) {
 
 			case reflect.Slice:
 				s := reflect.ValueOf(f.Interface())
-				if s.Index(0).Kind() == reflect.Struct {
+				if s.Len() > 0 && s.Index(0).Kind() == reflect.Struct {
 					log.Printf("    %s:\n", entries.Type().Field(i).Name)
 					for j := 0; j < s.Len(); j++ {
 						if s.Index(j).Kind() == reflect.Struct {
