de48cdd135379902689030475ffb12fda9099541
Frédéric Danis <frederic.danis@collabora.com>
2019-03-20 15:52:19 +0100

    docker: Update README
    Move build and run explanations from scripts to README.md
    
    Signed-off-by: Frédéric Danis <frederic.danis@collabora.com>

--- docker/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker run --rm -it \
-    -u $(id -u) \
-    --device /dev/kvm \
-    --group-add=$(getent group kvm | cut -d : -f 3) \
-    -v ${PWD}:/root \
-    debos \
-    /bin/bash -c "debos $*"
