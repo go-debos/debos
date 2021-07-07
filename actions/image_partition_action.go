go-debos/debos

commit a7afc01562bf7cedc4d36f2ef127c62a716a4f84

actions/image-partition: Consistently use artifactdir
The "image-partition" action uses artifactdir in the
PostMachineCleanup() step but not during the PreMachine() and
PreNoMachine() steps. Also, other debos actions such as pack, unpack
are relative to the specified "artifactdir" when invoking debos.

Fix this inconsistency by bringing image-partition action to the
fold. Also update the documentation to clarify this.

Signed-off-by: Punit Agrawal <punit1.agrawal@toshiba.co.jp>

actions/image_partition_action.go
@@ -20,7 +20,7 @@ Yaml syntax:
 
 Mandatory properties:
 
-- imagename -- the name of the image file.
+- imagename -- the name of the image file, relative to the artifact directory.
 
 - imagesize -- generated image size in human-readable form, examples: 100MB, 1GB, etc.
 
@@ -287,7 +287,8 @@ func (i *ImagePartitionAction) triggerDeviceNodes(context *debos.DebosContext) e
 
 func (i ImagePartitionAction) PreMachine(context *debos.DebosContext, m *fakemachine.Machine,
 	args *[]string) error {
-	image, err := m.CreateImage(i.ImageName, i.size)
+	ImagePath := path.Join(context.Artifactdir, i.ImageName)
+	image, err := m.CreateImage(ImagePath, i.size)
 	if err != nil {
 		return err
 	}
@@ -367,7 +368,8 @@ func (i ImagePartitionAction) formatPartition(p *Partition, context debos.DebosC
 
 func (i *ImagePartitionAction) PreNoMachine(context *debos.DebosContext) error {
 
-	img, err := os.OpenFile(i.ImageName, os.O_WRONLY|os.O_CREATE, 0666)
+	ImagePath = path.Join(context.Artifactdir, i.ImageName)
+	img, err := os.OpenFile(ImagePath, os.O_WRONLY|os.O_CREATE, 0666)
 	if err != nil {
 		return fmt.Errorf("Couldn't open image file: %v", err)
 	}
@@ -379,7 +381,7 @@ func (i *ImagePartitionAction) PreNoMachine(context *debos.DebosContext) error {
 
 	img.Close()
 
-	i.loopDev, err = losetup.Attach(i.ImageName, 0, false)
+	i.loopDev, err = losetup.Attach(ImagePath, 0, false)
 	if err != nil {
 		return fmt.Errorf("Failed to setup loop device")
 	}

commit e2d25f17bb93e6f5a7e68b63d29359410ccc4407

actions/image-partition: define ImagePath variable
The variable hasn't been defined yet so ends up producing
a build failure:

    actions/image_partition_action.go:371:2: undefined: ImagePath
    actions/image_partition_action.go:372:26: undefined: ImagePath
    actions/image_partition_action.go:384:34: undefined: ImagePath

Fix the syntax used to initialise this variable.

Fixes: a7afc01562bf ("actions/image-partition: Consistently use artifactdir")

Signed-off-by: Christopher Obbard <chris.obbard@collabora.com>

actions/image_partition_action.go
@@ -368,7 +368,7 @@ func (i ImagePartitionAction) formatPartition(p *Partition, context debos.DebosC
 
 func (i *ImagePartitionAction) PreNoMachine(context *debos.DebosContext) error {
 
-	ImagePath = path.Join(context.Artifactdir, i.ImageName)
+	ImagePath := path.Join(context.Artifactdir, i.ImageName)
 	img, err := os.OpenFile(ImagePath, os.O_WRONLY|os.O_CREATE, 0666)
 	if err != nil {
 		return fmt.Errorf("Couldn't open image file: %v", err)