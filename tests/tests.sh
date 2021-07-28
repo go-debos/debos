532beeff0d2347156dc37922a28ea766cab418e4
Vignesh Raman <vignesh.raman@collabora.com>
2021-07-28 12:50:19 +0530

    tests: add tests for "image-partition" action
    Add recipe file to test "image-partition" action.
    
    Signed-off-by: Vignesh Raman <vignesh.raman@collabora.com>

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-for dir in simple subdirs separatedirs ; do
+for dir in simple subdirs separatedirs image; do
     pushd ${dir}
     debos $@ main.yaml
     debos $@ main.yaml < /dev/null
