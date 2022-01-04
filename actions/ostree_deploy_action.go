go-debos/debos

commit 9b648141392f46120b8d7c30ab9cd2afaed18e43

ostree-deploy: fix umount error
Sometimes umount fails after ostree-deploy with following error:
2019/08/26 14:08:51 ==== Deploying ostree onto image ====
2019/08/26 14:08:57 ==== List files on ostreetestarmhf ====
2019/08/26 14:08:59 Warning: Failed to get unmount /: device or resource busy
2019/08/26 14:08:59 Unmount failure can cause images being incomplete!

libostree keeps some information, like repo lock file descriptor, in thread
specific variables.
As Go Garbage Collector can be run in a thread different from the one
which created the libostree object, it may not been able to access the
object thread specific variables, preventing to free them correctly.
To prevent this, explicitly dereference libostree objects.

Signed-off-by: Frédéric Danis <frederic.danis@collabora.com>

actions/ostree_deploy_action.go
@@ -55,7 +55,6 @@ import (
 	"io"
 	"os"
 	"path"
-	"runtime"
 	"strings"
 
 	"github.com/go-debos/debos"
@@ -198,6 +197,11 @@ func (ot *OstreeDeployAction) Run(context *debos.DebosContext) error {
 		return err
 	}
 
-	runtime.GC()
+	/* libostree keeps some information, like repo lock file descriptor, in
+	 * thread specific variables. As GC can be run from another thread, it
+	 * may not been able to access this, preventing to free them correctly.
+	 * To prevent this, explicitly dereference libostree objects. */
+	dstRepo.Unref()
+	sysroot.Unref()
 	return nil
 }

commit c17abd0d202c2b7aa4644cd6eefa2945ce67ea7a

ostree-deploy: Unreference all libostree variables
Unreference the ostree origin variable which wasn't
yet unreferenced.

Defer the unreference of all of the variables which
could be created from libostree, so they are unrefrenced
in the case of an error path returning before the
Unref() method call.

Fixes: 9b648141392f4 ("ostree-deploy: fix umount error")
Signed-off-by: Christopher Obbard <chris.obbard@collabora.com>

actions/ostree_deploy_action.go
@@ -121,6 +121,12 @@ func (ot *OstreeDeployAction) Run(context *debos.DebosContext) error {
 	repoPath := "file://" + path.Join(context.Artifactdir, ot.Repository)
 
 	sysroot := ostree.NewSysroot(context.Rootdir)
+
+	/* libostree keeps some information, like repo lock file descriptor, in
+	 * thread specific variables. As GC can be run from another thread, it
+	 * may not been able to access this, preventing to free them correctly.
+	 * To prevent this, explicitly dereference libostree objects. */
+	defer sysroot.Unref()
 	err := sysroot.InitializeFS()
 	if err != nil {
 		return err
@@ -136,6 +142,7 @@ func (ot *OstreeDeployAction) Run(context *debos.DebosContext) error {
 	   so reopen by hand */
 	/* dstRepo, err := sysroot.Repo(nil) */
 	dstRepo, err := ostree.OpenRepo(path.Join(context.Rootdir, "ostree/repo"))
+	defer dstRepo.Unref()
 	if err != nil {
 		return err
 	}
@@ -180,7 +187,9 @@ func (ot *OstreeDeployAction) Run(context *debos.DebosContext) error {
 	}
 
 	origin := sysroot.OriginNewFromRefspec("origin:" + ot.Branch)
+	defer origin.Unref()
 	deployment, err := sysroot.DeployTree(ot.Os, revision, origin, nil, kargs, nil)
+	defer deployment.Unref()
 	if err != nil {
 		return err
 	}
@@ -197,11 +206,5 @@ func (ot *OstreeDeployAction) Run(context *debos.DebosContext) error {
 		return err
 	}
 
-	/* libostree keeps some information, like repo lock file descriptor, in
-	 * thread specific variables. As GC can be run from another thread, it
-	 * may not been able to access this, preventing to free them correctly.
-	 * To prevent this, explicitly dereference libostree objects. */
-	dstRepo.Unref()
-	sysroot.Unref()
 	return nil
 }