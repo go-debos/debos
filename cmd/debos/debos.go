d7cc0bbf7f39d487567541224afd0c999657eb19
Christopher Obbard <chris.obbard@collabora.com>
2022-01-05 11:49:39 +0000

    Move handleError function to public namespace
    Other parts of the codebase will need to use the handleError
    function so refactor it into the main debos namespace.
    
    Signed-off-by: Christopher Obbard <chris.obbard@collabora.com>

--- cmd/debos/debos.go
+++ cmd/debos/debos.go
@@ -15,17 +15,6 @@ import (
 	"github.com/jessevdk/go-flags"
 )
 
-func handleError(context *debos.DebosContext, err error, a debos.Action, stage string) bool {
-	if err == nil {
-		return false
-	}
-
-	context.State = debos.Failed
-	log.Printf("Action `%s` failed at stage %s, error: %s", a, stage, err)
-	debos.DebugShell(*context)
-	return true
-}
-
 func do_run(r actions.Recipe, context *debos.DebosContext) (success bool) {
 	for _, a := range r.Actions {
 		log.Printf("==== %s ====\n", a)
@@ -37,13 +26,13 @@ func do_run(r actions.Recipe, context *debos.DebosContext) (success bool) {
 		defer func(action debos.Action) {
 			err := action.Cleanup(context)
 
-			if handleError(context, err, action, "Cleanup") {
+			if debos.HandleError(context, err, action, "Cleanup") {
 				success = false
 			}
 		}(a)
 
 		// Check the state of Run method
-		if handleError(context, err, a, "Run") {
+		if debos.HandleError(context, err, a, "Run") {
 			return false
 		}
 	}
@@ -241,7 +230,7 @@ func main() {
 
 	for _, a := range r.Actions {
 		err = a.Verify(&context)
-		if handleError(&context, err, a, "Verify") {
+		if debos.HandleError(&context, err, a, "Verify") {
 			return
 		}
 	}
@@ -319,11 +308,11 @@ func main() {
 				err := action.PostMachineCleanup(&context)
 
 				// report errors but do not stop execution
-				handleError(&context, err, action, "PostMachineCleanup")
+				debos.HandleError(&context, err, action, "PostMachineCleanup")
 			}(a)
 
 			err = a.PreMachine(&context, m, &args)
-			if handleError(&context, err, a, "PreMachine") {
+			if debos.HandleError(&context, err, a, "PreMachine") {
 				return
 			}
 		}
@@ -343,7 +332,7 @@ func main() {
 
 		for _, a := range r.Actions {
 			err = a.PostMachine(&context)
-			if handleError(&context, err, a, "PostMachine") {
+			if debos.HandleError(&context, err, a, "PostMachine") {
 				return
 			}
 		}
@@ -359,11 +348,11 @@ func main() {
 				err := action.PostMachineCleanup(&context)
 
 				// report errors but do not stop execution
-				handleError(&context, err, action, "PostMachineCleanup")
+				debos.HandleError(&context, err, action, "PostMachineCleanup")
 			}(a)
 
 			err = a.PreNoMachine(&context)
-			if handleError(&context, err, a, "PreNoMachine") {
+			if debos.HandleError(&context, err, a, "PreNoMachine") {
 				return
 			}
 		}
@@ -386,7 +375,7 @@ func main() {
 	if !fakemachine.InMachine() {
 		for _, a := range r.Actions {
 			err = a.PostMachine(&context)
-			if handleError(&context, err, a, "PostMachine") {
+			if debos.HandleError(&context, err, a, "PostMachine") {
 				return
 			}
 		}
