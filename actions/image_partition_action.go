d7e09c6562f0f963e3c33c53fb30cd01674a4d62
Christopher Obbard <chris.obbard@collabora.com>
2022-04-20 18:28:21 +0100

    actions: image-partition: Remove extra warning about removing loop device
    When removing loop devices, we retry after a delay with
    a timeout. Each time the loop device removal fails, a
    warning message is printed to the log:
    
      "Loop dev couldn't remove could not remove, device in use, waiting"
    
    This is pointless and could confuse the user. An error
    while performing the loop device removal & retry is
    logged seperately so let's remove the duplicate (and
    confusing) log message.
    
    Signed-off-by: Christopher Obbard <chris.obbard@collabora.com>

--- actions/image_partition_action.go
+++ actions/image_partition_action.go
@@ -604,7 +604,6 @@ func (i ImagePartitionAction) Cleanup(context *debos.DebosContext) error {
 			if err == nil {
 				break
 			}
-			log.Printf("Loop dev couldn't remove %s, waiting", err)
 			time.Sleep(time.Second)
 		}
 
