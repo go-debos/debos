go-debos/debos

commit 74df488fb6b0397de6d304000eabb1e5be3e66a2

actions: Add recipe action
This action includes the recipe actions at the given path, and can
optionally overrides or adds new template variables.

To allow recipes to operate both stand-alone and when included, template
variable "included_recipe" is added and set to "true" to indicate that
the recipe is part of a longer chain.
This allows a recipe to skip the unpack step if it's not the first to be
executed or to optionally skip a pack action when it's not the last.

For compatibility, both the parent recipe and all included recipes have
to be for the same architecture.

Limitations of combined recipes are equivalent to limitations within a
single recipe (e.g. there can only be one image partition action).

Print-recipe option is saved in DebosContext to allow each "recipe action"
to print its source during Parse.

Signed-off-by: Frédéric Danis <frederic.danis@collabora.com>

actions/recipe_action.go
@@ -1,0 +1,143 @@
+/*
+Recipe Action
+
+Include a recipe.
+
+Yaml syntax:
+ - action: recipe
+   recipe: path to recipe
+   variables:
+     key: value
+
+Mandatory properties:
+
+- recipe -- includes the recipe actions at the given path.
+
+Optional properties:
+
+- variables -- overrides or adds new template variables.
+
+*/
+package actions
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"github.com/go-debos/debos"
+	"github.com/go-debos/fakemachine"
+)
+
+type RecipeAction struct {
+	debos.BaseAction `yaml:",inline"`
+	Recipe           string
+	Variables        map[string]string
+	Actions          Recipe `yaml:"-"`
+	templateVars     map[string]string
+}
+
+func (recipe *RecipeAction) Verify(context *debos.DebosContext) error {
+	if len(recipe.Recipe) == 0 {
+		return errors.New("'recipe' property can't be empty")
+	}
+
+	file := recipe.Recipe
+	if !filepath.IsAbs(file) {
+		file = filepath.Clean(context.RecipeDir + "/" + recipe.Recipe)
+	}
+
+	if _, err := os.Stat(file); os.IsNotExist(err) {
+		return err
+	}
+
+	// Initialise template vars
+	recipe.templateVars = make(map[string]string)
+	recipe.templateVars["included_recipe"] = "true"
+	recipe.templateVars["architecture"] = context.Architecture
+
+	// Add Variables to template vars
+	for k, v := range recipe.Variables {
+		recipe.templateVars[k] = v
+	}
+
+	if err := recipe.Actions.Parse(file, context.PrintRecipe, recipe.templateVars); err != nil {
+		return err
+	}
+
+	if context.Architecture != recipe.Actions.Architecture {
+		return fmt.Errorf("Expect architecture '%s' but got '%s'", context.Architecture, recipe.Actions.Architecture)
+	}
+
+	for _, a := range recipe.Actions.Actions {
+		if err := a.Verify(context); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (recipe *RecipeAction) PreMachine(context *debos.DebosContext, m *fakemachine.Machine, args *[]string) error {
+	// TODO: check args?
+
+	for _, a := range recipe.Actions.Actions {
+		if err := a.PreMachine(context, m, args); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (recipe *RecipeAction) PreNoMachine(context *debos.DebosContext) error {
+	for _, a := range recipe.Actions.Actions {
+		if err := a.PreNoMachine(context); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (recipe *RecipeAction) Run(context *debos.DebosContext) error {
+	recipe.LogStart()
+
+	for _, a := range recipe.Actions.Actions {
+		if err := a.Run(context); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (recipe *RecipeAction) Cleanup(context *debos.DebosContext) error {
+	for _, a := range recipe.Actions.Actions {
+		if err := a.Cleanup(context); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (recipe *RecipeAction) PostMachine(context *debos.DebosContext) error {
+	for _, a := range recipe.Actions.Actions {
+		if err := a.PostMachine(context); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (recipe *RecipeAction) PostMachineCleanup(context *debos.DebosContext) error {
+	for _, a := range recipe.Actions.Actions {
+		if err := a.PostMachineCleanup(context); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}

action.go
@@ -37,6 +37,7 @@ type DebosContext struct {
 	Origins         map[string]string
 	State           DebosState
 	EnvironVars     map[string]string
+	PrintRecipe     bool
 }
 
 type Action interface {

actions/recipe.go
@@ -57,6 +57,8 @@ Supported actions
 
 - raw -- https://godoc.org/github.com/go-debos/debos/actions#hdr-Raw_Action
 
+- recipe -- https://godoc.org/github.com/go-debos/debos/actions#hdr-Recipe_Action
+
 - run -- https://godoc.org/github.com/go-debos/debos/actions#hdr-Run_Action
 
 - unpack -- https://godoc.org/github.com/go-debos/debos/actions#hdr-Unpack_Action
@@ -118,6 +120,8 @@ func (y *YamlAction) UnmarshalYAML(unmarshal func(interface{}) error) error {
 		y.Action = &RawAction{}
 	case "download":
 		y.Action = &DownloadAction{}
+	case "recipe":
+		y.Action = &RecipeAction{}
 	default:
 		return fmt.Errorf("Unknown action: %v", aux.Action)
 	}

cmd/debos/debos.go
@@ -104,9 +104,15 @@ func DumpActionFields(iface interface{}, depth int) {
 		if f.CanInterface() {
 			switch f.Kind() {
 			case reflect.Struct:
-				// BaseAction is the only struct embbed in Action ActionFields
-				// dump it at the same level
-				log.Printf("  - %s", DumpActionStruct(f.Interface()))
+				switch entries.Type().Field(i).Type.String() {
+				case "debos.BaseAction":
+					// BaseAction is the only struct embbed in Action ActionFields
+					// dump it at the same level
+					log.Printf("%s- %s", tab, DumpActionStruct(f.Interface()))
+
+				case "actions.Recipe":
+					DumpActions(f.Interface(), depth + 1)
+				}
 
 			case reflect.Slice:
 				s := reflect.ValueOf(f.Interface())
@@ -187,6 +193,10 @@ func main() {
 	// Set interactive shell binary only if '--debug-shell' options passed
 	if options.DebugShell {
 		context.DebugShell = options.Shell
+	}
+
+	if options.PrintRecipe {
+		context.PrintRecipe = options.PrintRecipe
 	}
 
 	file := args[0]

commit a9919aa8da7bba0f05e5aea1542923e52501c296

actions/recipe: Only run cleanup function if needed
When running nested recipes using the recipe action,
if an action in the child recipe exits early (e.g. on
failure) the remaining actions do not run but the
cleanup functions for these remaining actions still
run even though there is nothing to cleanup.

This doesn't follow how Debos handles running standalone
recipes, so modify the recipe action to only run action
cleanup functions when the associated action needs to be
cleaned up.

Fixes: 74df488fb6b0 ("actions: Add recipe action")
Signed-off-by: Christopher Obbard <chris.obbard@collabora.com>

actions/recipe_action.go
@@ -45,6 +45,9 @@ type RecipeAction struct {
 	Actions          Recipe `yaml:"-"`
 	templateVars     map[string]string
 	context          debos.DebosContext
+
+	cleanupActions            []YamlAction
+	postMachineCleanupActions []YamlAction
 }
 
 func (recipe *RecipeAction) Verify(context *debos.DebosContext) error {
@@ -96,6 +99,8 @@ func (recipe *RecipeAction) PreMachine(context *debos.DebosContext, m *fakemachi
 	m.AddVolume(recipe.context.RecipeDir)
 
 	for _, a := range recipe.Actions.Actions {
+		recipe.postMachineCleanupActions = append(recipe.postMachineCleanupActions, a)
+
 		if err := a.PreMachine(&recipe.context, m, args); err != nil {
 			return err
 		}
@@ -106,6 +111,8 @@ func (recipe *RecipeAction) PreMachine(context *debos.DebosContext, m *fakemachi
 
 func (recipe *RecipeAction) PreNoMachine(context *debos.DebosContext) error {
 	for _, a := range recipe.Actions.Actions {
+		recipe.postMachineCleanupActions = append(recipe.postMachineCleanupActions, a)
+
 		if err := a.PreNoMachine(&recipe.context); err != nil {
 			return err
 		}
@@ -116,6 +123,8 @@ func (recipe *RecipeAction) PreNoMachine(context *debos.DebosContext) error {
 
 func (recipe *RecipeAction) Run(context *debos.DebosContext) error {
 	for _, a := range recipe.Actions.Actions {
+		recipe.cleanupActions = append(recipe.cleanupActions, a)
+
 		log.Printf("==== %s ====\n", a)
 		if err := a.Run(&recipe.context); err != nil {
 			return err
@@ -126,7 +135,8 @@ func (recipe *RecipeAction) Run(context *debos.DebosContext) error {
 }
 
 func (recipe *RecipeAction) Cleanup(context *debos.DebosContext) (err error) {
-	for _, a := range recipe.Actions.Actions {
+	/* only run Cleanup if Run was attempted */
+	for _, a := range recipe.cleanupActions {
 		defer func(action debos.Action) {
 			cleanup_err := action.Cleanup(context)
 
@@ -152,7 +162,8 @@ func (recipe *RecipeAction) PostMachine(context *debos.DebosContext) error {
 }
 
 func (recipe *RecipeAction) PostMachineCleanup(context *debos.DebosContext) (err error) {
-	for _, a := range recipe.Actions.Actions {
+	/* only run PostMachineCleanup if PreNoMachine OR PreMachine was attempted */
+	for _, a := range recipe.postMachineCleanupActions {
 		defer func(action debos.Action) {
 			cleanup_err := action.PostMachineCleanup(context)
 