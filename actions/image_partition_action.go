2a40c09840b79f9b1641804944d27165354cdfb8
Loïc Minier <loic.minier@oss.qualcomm.com>
2025-09-05 13:07:26 +0000

    fix: struct field tag not compatible with reflect.StructTag.Get
    Reported by go vet
    
    Signed-off-by: Loïc Minier <loic.minier@oss.qualcomm.com>

--- actions/image_partition_action.go
+++ actions/image_partition_action.go
@@ -215,7 +215,7 @@ type Partition struct {
 	Flags           []string
 	Features        []string
 	ExtendedOptions []string
-	Fsck            bool "fsck"
+	Fsck            bool   `yaml:"fsck"`
 	FSUUID          string
 }
 
@@ -250,7 +250,7 @@ type ImagePartitionAction struct {
 	ImageSize        string
 	PartitionType    string
 	DiskID           string
-	GptGap           string "gpt_gap"
+	GptGap           string   `yaml:"gpt_gap"`
 	Partitions       []Partition
 	Mountpoints      []Mountpoint
 	size             int64
