e2c02381315b8faf14728de261117000826e4dd0
Christopher Obbard <chris.obbard@oss.qualcomm.com>
2026-06-22 08:05:07 +0100

    tests: add TestBasicCommandFailure
    Cover the failure path of `Command.Run` with a command that cannot be
    executed to confirm that Run returns a non-nil error.
    
    Signed-off-by: Christopher Obbard <chris.obbard@oss.qualcomm.com>

--- commands_test.go
+++ commands_test.go
@@ -10,3 +10,8 @@ func TestBasicCommand(t *testing.T) {
 	err := Command{}.Run("out", "ls", "-l")
 	require.NoError(t, err)
 }
+
+func TestBasicCommandFailure(t *testing.T) {
+	err := Command{}.Run("out", "this-command-does-not-exist")
+	require.Error(t, err)
+}
